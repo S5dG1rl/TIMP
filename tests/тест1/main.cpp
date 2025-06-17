8b6f504e8d170b4096ba5f4ce707b7e5f6f084f2
nast2007 <ya.nastyaer@yandex.ru>
2025-06-18 01:24:57 +0300

    Update main.cpp

--- tests/тест1/main.cpp
+++ tests/тест1/main.cpp
@@ -1,11 +1,12 @@
 #include <QCoreApplication>
 #include <QtTest>
+
 #include "sha512tests.h"
 
-int main(int argc, char *argv[])
-{
-    QCoreApplication a(argc, argv);
+int main(int argc, char *argv[]) {
+    QCoreApplication app(argc, argv);
 
     Sha512Tests tests;
-    return QTest::qExec(&tests);
+    int result = QTest::qExec(&tests, argc, argv);
+    return result;
 }
