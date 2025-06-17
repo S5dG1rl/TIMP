f16b4aa4312176bbcc3c47f7aad3e084863159f9
nast2007 <ya.nastyaer@yandex.ru>
2025-06-17 23:49:35 +0300

    Create sha512tests.h

---
+++ tests/тест1/sha512tests.h
@@ -0,0 +1,15 @@
+#ifndef SHA512TESTS_H
+#define SHA512TESTS_H
+
+#include <QObject>
+
+class Sha512Tests : public QObject
+{
+    Q_OBJECT
+
+private slots:
+    void test_sha512_emptyString();
+    void test_sha512_simpleText();
+};
+
+#endif // SHA512TESTS_H
